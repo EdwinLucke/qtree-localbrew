031f2de1011693a46bcacaa30d1ec9fd620659d2
@win <42358844+EdwinLucke@users.noreply.github.com>
2018-11-20 22:40:13 +0100

    pull from develop to master (#4)
    * swagger added
    
    * add changes to dev
    
    * do it all
    
    * default response class added . . moved to different project

---
+++ qtree.website/qtree.core.msunittest/UnitTest1.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using qtree.core.common;
+using qtree.core.website;
+using System;
+using System.ComponentModel;
+
+namespace qtree.core.msunittest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var dataObj = ApiResponse.DataResponse("test");
+            foreach (PropertyDescriptor prop in TypeDescriptor.GetProperties(dataObj))
+            {
+                Console.WriteLine($"{prop.Name}={prop.GetValue(dataObj)} - {prop.Attributes.ToString()}");
+            }
+
+            var theT = dataObj.GetType();
+            //CustomDataContractResolver.Instance < dataObj.GetType() >.CreateProperty()
+            //CustomDataContractResolver<dataObj>.Instance.CreateProperty(typeof(dataObj))
+            Console.WriteLine(ApiResponse.DataResponse("test"));
+        }
+    }
+}
---
+++ qtree.website/qtree.core.website/Controllers/MyFirstController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using qtree.core.common;
+
+namespace qtree.core.website.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyFirstController : ControllerBase
+    {
+        /// <summary>
+        /// Samenvatting
+        /// </summary>
+        /// <returns>200 | OK ; the data is returned in the data object</returns>
+        // GET: api/MyFirst
+        [HttpGet]
+        public ObjectResult Get()
+        {
+            // ApiResponse.
+            return Ok(ApiResponse.DataResponse(new string[] { "value1", "value2" }));
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/MyFirst/5
+        [HttpGet("{id}", Name = "Get")]
+        public ObjectResult Get(int id)
+        {
+            try
+            {
+                if (id == 2) throw new EntryPointNotFoundException();
+                return Ok(ApiResponse.DataResponse($"ID value={id}"));
+            }catch(Exception ex)
+            {
+                return Ok(ApiResponse.Error(ex, $"ID value={id}"));
+            }
+        }
+
+        // POST: api/MyFirst
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/MyFirst/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
